1ced0aee32b16783bd1d16f6c530c79b8922c26b
te170476 <te170476@gmail.com>
2020-07-20 14:58:10 +0900

    Add interface IEquipmentService

---
+++ src/main/java/jp/ac/jc_21/stupidunion/equipmeneger/service/IEquipmentService.java
@@ -0,0 +1,13 @@
+package jp.ac.jc_21.stupidunion.equipmeneger.service;
+
+import java.util.List;
+
+import jp.ac.jc_21.stupidunion.equipmeneger.formdata.EquipmentFormData;
+
+public interface IEquipmentService {
+	public EquipmentFormData create(EquipmentFormData formData);
+	public EquipmentFormData update(EquipmentFormData formData);
+	public void delete(Integer id);
+	public List<EquipmentFormData> findAll();
+	public EquipmentFormData findOne(Integer id);
+}
