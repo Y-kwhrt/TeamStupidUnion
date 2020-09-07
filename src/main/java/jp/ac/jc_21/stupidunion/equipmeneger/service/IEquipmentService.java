906548196605426c10b72b526204fdb388a50a6f
megu <47018641+170154@users.noreply.github.com>
2020-09-07 12:57:35 +0900

    EquipmentFormData.javaのlijespanInYearsの型をStringからintに戻しました。 (#24)
    それに伴いDigitsとDigitsのエラーメッセージが不要になったので削除しました。
    list.htmlの耐用年数のinput typeをtextからnumberに変更しました。
    
    Co-authored-by: 170154 <170154@e0412-03.JC-21.local>

--- src/main/java/jp/ac/jc_21/stupidunion/equipmeneger/service/IEquipmentService.java
+++ src/main/java/jp/ac/jc_21/stupidunion/equipmeneger/service/IEquipmentService.java
@@ -6,8 +6,12 @@ import jp.ac.jc_21.stupidunion.equipmeneger.formdata.EquipmentFormData;
 
 public interface IEquipmentService {
 	public EquipmentFormData create(EquipmentFormData formData);
+
 	public EquipmentFormData update(EquipmentFormData formData);
+
 	public void delete(Integer id);
+
 	public List<EquipmentFormData> findAll();
+
 	public EquipmentFormData findOne(Integer id);
 }
